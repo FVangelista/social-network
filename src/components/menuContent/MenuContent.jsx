c34e58048f133a76ac5933ce77794955c9abf155
Filippo Bortoletti <filippobortoletti@MBP-di-Filippo.home>
2023-03-08 22:15:59 +0100

    responsive update

--- src/components/menuContent/MenuContent.jsx
+++ src/components/menuContent/MenuContent.jsx
@@ -1,26 +1,49 @@
+import { IoLogoTwitter } from 'react-icons/io';
+import { FaHashtag, FaEnvelope, FaRegBookmark } from 'react-icons/fa';
+import { RiHome7Fill, RiNotification2Line } from 'react-icons/ri';
 import './index.css';
 
 const MenuContent = () => {
   return (
     <div className="MenuContent">
       <ul>
-        <li>
-          <a href="#">home</a>
+        <li className="main-logo">
+          <IoLogoTwitter className="logo" />
         </li>
         <li>
-          <a href="#">explore</a>
+          <a href="#">
+            <RiHome7Fill className="logo" />
+            <p className="menu-text">home</p>
+          </a>
         </li>
         <li>
-          <a href="#">notifications</a>
+          <a href="#">
+            <FaHashtag className="logo" />
+            <p className="menu-text">explore</p>
+          </a>
         </li>
         <li>
-          <a href="#">messages</a>
+          <a href="#">
+            <RiNotification2Line className="logo" />
+            <p className="menu-text">notifications</p>
+          </a>
         </li>
         <li>
-          <a href="#">bookmarks</a>
+          <a href="#">
+            <FaEnvelope className="logo" />
+            <p className="menu-text">messages</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FaRegBookmark className="logo" />
+            <p className="menu-text">bookmarks</p>
+          </a>
+        </li>
+        <li>
+          <button className="tweet-btn">tweet</button>
         </li>
       </ul>
-      <button className="tweet-btn">tweet</button>
     </div>
   );
 };
